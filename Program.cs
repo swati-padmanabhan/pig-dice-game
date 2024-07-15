15440e312512181479cc5036826fb507b50c83b7
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-15 20:25:40 +0530

    updated the program.

--- Program.cs
+++ Program.cs
@@ -4,40 +4,41 @@ namespace PigDiceGame
 {
     internal class Program
     {
-        const int WinningScore = 20; //Score needed to win the game
-        const int MinDiceValue = 1;  //Minimum value of dice
-        const int MaxDiceValue = 6;  //Maximum value of dice
+        const int WinningScore = 20; // Score needed to win the game
+        const int MinDiceValue = 1;  // Minimum value of dice
+        const int MaxDiceValue = 6;  // Maximum value of dice
 
-        int overallScore = 0;        //Initial score of the player
-        int currentTurn = 1;         //Current turn
-        Random dice = new Random();  //Random number generator for dice
+        int overallScore = 0;        // Initial score of the player
+        int currentTurn = 1;         // Current turn
+        Random dice = new Random();  // Random number generator for dice
 
         public static void Main(string[] args)
         {
-            Program game = new Program();  //Create an instance of the program
+            Program game = new Program();  // Create an instance of the program
             game.PlayGame();  // Call the PlayGame method on the instance to start the game
+
+            Console.ReadLine();
         }
 
         public void PlayGame()
         {
             Console.WriteLine("\nWelcome to the Pig Dice Game!");
 
-
-            //This loop continues until the user reaches the Winning Score
+            // This loop continues until the user reaches the Winning Score
             while (overallScore < WinningScore)
             {
                 HandlePlayerTurn();
-                //Increment the turn value after each turn
-                currentTurn++;
+                currentTurn++; // Increment the turn value after each turn
             }
 
+            // This line will now execute after the loop exits
             Console.WriteLine($"You Win! You finished in {currentTurn - 1} turns!");
             Console.WriteLine("Game ends!");
         }
 
         public void HandlePlayerTurn()
         {
-            int turnPoints = 0;     //Intially the accumulated points would be set to zero
+            int turnPoints = 0;     // Initially the accumulated points would be set to zero
             bool isTurnOver = false;
 
             Console.WriteLine("\n===============================================================");
@@ -55,22 +56,21 @@ namespace PigDiceGame
 
                     if (diceRoll == MinDiceValue)
                     {
-                        //If the player rolls a 1, end the turn with no score
+                        // If the player rolls a 1, end the turn with no score
                         Console.WriteLine("Turn over. No Score");
                         isTurnOver = true;
                     }
                     else
                     {
-                        //Add the dice roll to the turn score
+                        // Add the dice roll to the turn score
                         turnPoints += diceRoll;
                         Console.WriteLine($"Your turn score is {turnPoints} and your total score is {overallScore}");
                         Console.WriteLine($"If you hold, you will have {overallScore + turnPoints} points.");
-                        
                     }
                 }
                 else if (inputChoice == "h")
                 {
-                    //If the player chooses h, add the turn score to the overall score
+                    // If the player chooses h, add the turn score to the overall score
                     overallScore += turnPoints;
                     Console.WriteLine($"Your turn score is {turnPoints} and your total score is {overallScore}");
                     isTurnOver = true;
@@ -84,7 +84,7 @@ namespace PigDiceGame
 
         public int RollDice()
         {
-            //Generate a random number
+            // Generate a random number
             return dice.Next(MinDiceValue, MaxDiceValue + 1);
         }
     }
